89f1ccaaa350e39338320bbe6483fa2dac3637ef
MIRZA USAMA ALI BAIG <64379502+mirzaghalib4726@users.noreply.github.com>
2023-07-11 14:04:21 +0500

    Created reactForm.js

---
+++ reactForm.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+
+function Form() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Send form data to 127.0.0.1/formData
+    const formData = {
+      name: name,
+      email: email
+    };
+
+    fetch('http://127.0.0.1/formData', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Handle response data
+        console.log(data);
+      })
+      .catch(error => {
+        // Handle errors
+        console.error(error);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={handleNameChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default Form;
